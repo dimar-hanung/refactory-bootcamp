8e0667a894affcc40914e1dfaa8522275f616693
dimar hanung <42312636+dimar-hanung@users.noreply.github.com>
2020-08-14 00:42:34 +0700

    chore(release): :tada: 1.0.0

---
+++ day-19/tailwind-install/tailwind.config.js
@@ -0,0 +1,26 @@
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [
+    plugin(function({ addComponents }){
+      const buttons = {
+        '.btn-red': {
+          padding: '.5rem 1rem',
+          borderRadius: '.25rem',
+          fontWeight: '600',
+          backgroundColor: '#e3342f',
+          color: '#fff',
+          '&:hover': {
+            backgroundColor: '#cc1f1a'
+          }
+        }
+      }
+      
+      addComponents(buttons)
+    })
+  ],
+}
