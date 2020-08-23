220ba2f77c7265dabb1aeb296dbda83209b21a33
dimar hanung <42312636+dimar-hanung@users.noreply.github.com>
2020-08-23 23:57:47 +0700

    chore(release): :tada: vue-cli + router + tailwindcss hello-world

---
+++ day-24/hello-world/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+    <!-- <t-button>Press me</tbutton> -->
+    <h1 class="p-2 ml-4 rounded text-red-700 hover:text-red-600">bruh</h1>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ day-24/hello-world/src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>Perkenalkan Nama Saya Dimar</h1>
+    <button class="p-2 ml-4 rounded text-red-700 hover:text-red-600">dimar</button>
+  </div>
+</template>
