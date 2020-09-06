f322fadaf2e9ab9c16b1b1a2583dacf94ceb28d9
dimar hanung <42312636+dimar-hanung@users.noreply.github.com>
2020-09-06 22:08:11 +0700

    refactor(task): :recycle: now use requests

--- day-35/05-combine.py
+++ day-35/05-combine.py
@@ -1,14 +1,11 @@
-import urllib.request, json
+import requests, json
 
-with urllib.request.urlopen("https://jsonplaceholder.typicode.com/posts") as url:
-    posts = json.loads(url.read().decode())
-
-with urllib.request.urlopen("https://jsonplaceholder.typicode.com/users") as url:
-    users = json.loads(url.read().decode())
+posts = requests.get("https://jsonplaceholder.typicode.com/posts").json()
+users = requests.get("https://jsonplaceholder.typicode.com/users").json()
 
 def user(id):
     return list(filter(lambda a: a['id'] == id, users))[0]
 
 for post in posts:
     post['user'] = user(post['userId'])
-    print(json.dumps(post, indent=1))
+    print(json.dumps(post, indent=4))
