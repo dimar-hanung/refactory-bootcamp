e45ded061c9b3bf720d3139ec4d7b59bf3e3b14b
dimar hanung <42312636+dimar-hanung@users.noreply.github.com>
2020-09-06 19:04:41 +0700

    refactor(task): :recycle: now with response message

--- day-35/06-fetch.py
+++ day-35/06-fetch.py
@@ -1,12 +1,23 @@
-import requests
+import requests,json
 from bs4 import BeautifulSoup
 
 url = 'https://indeks.kompas.com/headline'
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
+res = requests.get(url)
+soup = BeautifulSoup(res.content, 'html.parser')
 
-elemt = soup.find_all('a', class_="article__link", href=True)
-title = [t.text for t in elemt]
-url = [u['href'] for u in elemt]
-for i in range(len(title)):
-    print({'URL': url[i], 'Title': title[i]})
+elems = soup.find_all('a', class_="article__link", href=True)
+
+response = {
+    "status":res.status_code,
+    "message":"Berhasil mendapatkan data",
+    "data": []
+}
+
+for elem in elems:
+    response['data'].append({'Title': elem.text,'URL': elem['href']})
+
+print(json.dumps(response,indent = 4))
+
+f = open("data.json", "w") # membuat file
+f.write(json.dumps(response,indent = 4)) # menulis file \n = ganti baris
+f.close() # tutup
