9ec08c3966082f95342bab8283221ee11becede4
dimar hanung <42312636+dimar-hanung@users.noreply.github.com>
2020-09-08 00:54:54 +0700

    feat(add): :sparkles: message if false

--- day-36/test/test_a_count.py
+++ day-36/test/test_a_count.py
@@ -6,13 +6,13 @@ from a_count import countString
 
 class countStringTest(unittest.TestCase):
     def test_dimar(self):
-        self.assertEqual(countString('dimar'), 5)
+        self.assertEqual(countString('dimar'), 5, msg="dimar seharusnya 5 huruf")
 
     def test_hanung(self):
-        self.assertEqual(countString('hanung'), 6)
+        self.assertEqual(countString('hanung'), 6, msg="hanung seharusnya 6 huruf")
     
     def test_not_str(self):
-        self.assertFalse(countString([1,2,3]))
+        self.assertFalse(countString([1,2,3]) or countString(1) or countString({"s"}),msg="selain string seharusnya false")
 
 if __name__ == '__main__':
     unittest.main()
