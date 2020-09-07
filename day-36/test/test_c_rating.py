aa8ceca5f2cf1abd26f50642062afd000122a4a0
dimar hanung <42312636+dimar-hanung@users.noreply.github.com>
2020-09-08 00:53:13 +0700

    feat(add): :sparkles: message if false

--- day-36/test/test_c_rating.py
+++ day-36/test/test_c_rating.py
@@ -6,16 +6,19 @@ from c_rating import rating
 
 class ratingTest(unittest.TestCase):
     def test_13(self):
-        self.assertEqual(rating(13), "REMAJA")
+        self.assertEqual(rating(13), "REMAJA",msg="13 untuk remaja")
 
     def test_21(self):
-        self.assertEqual(rating(21), "DEWASA")
+        self.assertEqual(rating(21), "DEWASA",msg="21 untuk dewasa")
     
     def test_9(self):
-        self.assertEqual(rating(9), "BIMBINGAN ORANG TUA")
+        self.assertEqual(rating(9), "BIMBINGAN ORANG TUA",msg="9 butuh bimbingan")
     
     def test_8(self):
         self.assertEqual(rating(8), "SEMUA USIA")
+
+    def test_500(self):
+        self.assertRaises(ValueError,rating,1000)
 
     def test_str(self):
         self.assertFalse(rating('string'))
