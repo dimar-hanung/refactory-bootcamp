75a4649dccd4296276bf4629cf6bf25df31fa478
dimar hanung <42312636+dimar-hanung@users.noreply.github.com>
2020-09-08 07:37:48 +0700

    refactor: change variable name

--- day-36/src/e_convert.py
+++ day-36/src/e_convert.py
@@ -3,23 +3,23 @@ def num2word(bil):
         return False
     angka = ["","Satu","Dua","Tiga","Empat","Lima","Enam",
              "Tujuh","Delapan","Sembilan","Sepuluh","Sebelas"]
-    Hasil = " "
+    hasil = " "
     n = int(bil)
     if n>= 0 and n <= 11:
-        Hasil = angka[n]
+        hasil = angka[n]
     elif n <20:
-        Hasil = num2word (n-10) + " Belas "
+        hasil = num2word (n-10) + " Belas "
     elif n <100:
-        Hasil = num2word (n/10) + " Puluh " + num2word (n%10)
+        hasil = num2word (n/10) + " Puluh " + num2word (n%10)
     elif n <200:
-        Hasil = " Seratus " + num2word (n-100)
+        hasil = " Seratus " + num2word (n-100)
     elif n <1000:
-        Hasil = num2word (n/100) + " Ratus " + num2word (n%100)
+        hasil = num2word (n/100) + " Ratus " + num2word (n%100)
     elif n <2000:
-        Hasil = " Seribu " + num2word (n-1000)
+        hasil = " Seribu " + num2word (n-1000)
     else:
-        Hasil = "Angka Hanya Sampai 1999"
+        hasil = "Angka Hanya Sampai 1999"
 
-    return Hasil.strip()
+    return hasil.strip()
 
-print(num2word(2000))
+print(num2word(1234))
