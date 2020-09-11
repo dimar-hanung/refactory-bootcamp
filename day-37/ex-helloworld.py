e870ba208333f2a6523207231b7abc14412473f3
dimar hanung <42312636+dimar-hanung@users.noreply.github.com>
2020-09-12 00:02:31 +0700

    feat(ex): update example

--- day-37/ex-helloworld.py
+++ day-37/ex-helloworld.py
@@ -1,13 +1,16 @@
 import click
 
 @click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def hello(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo('Hello %s!' % name)
+@click.argument("name",type=click.STRING)
+@click.option('--city', default='kosong', help='Number of greetings.')
+@click.option('--male',type=click.STRING, default=False,is_flag=True, help='Number of greetings.')
+def hello(name,city,male):
+    print("name:",name)
+    print("city:",city)
+    print("dia laki laki:",male)
+
+# contoh manggil :
+# python .\ex-helloworld.py dimar --city purwokerto --male
 
 if __name__ == '__main__':
     hello()
