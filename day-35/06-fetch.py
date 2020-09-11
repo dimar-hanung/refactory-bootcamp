a118d9a86fccbb5b851d53b4cfbc8b290e9bde74
dimar hanung <42312636+dimar-hanung@users.noreply.github.com>
2020-09-12 00:00:38 +0700

    feat(task): add code to function

--- day-35/06-fetch.py
+++ day-35/06-fetch.py
@@ -1,23 +1,20 @@
 import requests,json
 from bs4 import BeautifulSoup
 
-url = 'https://indeks.kompas.com/headline'
-res = requests.get(url)
-soup = BeautifulSoup(res.content, 'html.parser')
+def scrap(url):
+    res = requests.get(url)
+    soup = BeautifulSoup(res.content, 'html.parser')
+    elems = soup.find_all('a', class_="article__link", href=True)
+    response = {
+        "status":res.status_code,
+        "message":"Berhasil mendapatkan data",
+        "data": []
+    }
+    for elem in elems:
+        response['data'].append({'Title': elem.text,'URL': elem['href']})
+    print(json.dumps(response,indent = 4))
+    f = open("data.json", "w") # membuat file
+    f.write(json.dumps(response,indent = 4)) # menulis file \n = ganti baris
+    f.close() # tutup
 
-elems = soup.find_all('a', class_="article__link", href=True)
-
-response = {
-    "status":res.status_code,
-    "message":"Berhasil mendapatkan data",
-    "data": []
-}
-
-for elem in elems:
-    response['data'].append({'Title': elem.text,'URL': elem['href']})
-
-print(json.dumps(response,indent = 4))
-
-f = open("data.json", "w") # membuat file
-f.write(json.dumps(response,indent = 4)) # menulis file \n = ganti baris
-f.close() # tutup
+scrap('https://indeks.kompas.com/headline')
