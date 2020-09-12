36dbcd346a6c47ad06b75dd6ac7a210ea42e02fb
dimar hanung <42312636+dimar-hanung@users.noreply.github.com>
2020-09-12 14:04:35 +0700

    refactor(task): adding error code & solve bug

--- day-40/03-save.sh
+++ day-40/03-save.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 filename=$1
 cat $filename | while read LINE; do
+{
     IFS='/' read -ra SOURCE <<< $LINE
     file=${SOURCE[-1]}
-    data=$(curl -s $LINE)
-    echo $data | jq . > data/$file.json
-    echo "${file}.json created"
-done
+    response=$(curl -s -w "\n%{http_code}" $LINE?format=json) exitCode=$?
+    echo $'\n'
+    echo $LINE | cat -v
+    array=(${response[@]}) # convert to array
+    code=${array[-1]} # get last element (last line)
+
+    if [[ "$code" == "200" ]];then
+        # body=${response[@]} # get all elements except last
+        body=${array[@]::${#array[@]}-1}
+        echo $code
+        echo $body | jq .
+        echo $body | jq . > data/${file}.json
+        echo "${file}.json created"
+    else 
+        echo $response
+    fi
+
+    
+};
+done <"$filename"
+
