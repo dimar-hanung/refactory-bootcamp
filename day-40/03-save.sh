8bb47938758e1ce11991acbad585df3db498f22d
dimar hanung <42312636+dimar-hanung@users.noreply.github.com>
2020-09-12 10:18:51 +0700

    feat(task): save file to data folder

--- day-40/03-save.sh
+++ day-40/03-save.sh
@@ -4,6 +4,6 @@ cat $filename | while read LINE; do
     IFS='/' read -ra SOURCE <<< $LINE
     file=${SOURCE[-1]}
     data=$(curl -s $LINE)
-    echo $data > X.json
+    echo $data | jq . > data/$file.json
     echo "${file}.json created"
 done
