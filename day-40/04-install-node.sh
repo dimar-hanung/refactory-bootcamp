86ea0d82d9cedbb0f23e554cb38ae4ff30118609
dimar hanung <42312636+dimar-hanung@users.noreply.github.com>
2020-09-14 07:52:05 +0700

    docs(comment): remove

--- day-40/04-install-node.sh
+++ day-40/04-install-node.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-: '
-Cara mengeceknya :
-    node --version,
-    jika version yg sudah ada lebih kecil
-Jika version yang sudah ada lebih besar,
-maka node version yg akan diinstall,
-tidak akan bisa me-replace node versionnya
-'
 
 if [[ $1 == 13 || $1 == 12 || $1 == 11 || $1 == 10 || $1 == 8 ]]; then
     curl -sL https://deb.nodesource.com/setup_$1.x | sudo -E bash -
