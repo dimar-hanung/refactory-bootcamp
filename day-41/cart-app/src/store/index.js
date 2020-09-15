03e4b80381b883155f1329864c4e2f786b84a96c
dimar hanung <42312636+dimar-hanung@users.noreply.github.com>
2020-09-15 08:55:42 +0700

    fix: add 0 minimal count

--- day-41/cart-app/src/store/index.js
+++ day-41/cart-app/src/store/index.js
@@ -23,7 +23,9 @@ export default new Vuex.Store({
       if (cache) commit("setItems", JSON.parse(cache));
       else {
         axios
-          .get("https://jsonplaceholder.typicode.com/photos?_start=0&_limit=100")
+          .get(
+            "https://jsonplaceholder.typicode.com/photos?_start=0&_limit=100"
+          )
           .then((r) => r.data)
           .then((items) => {
             commit("setItems", items);
@@ -31,45 +33,47 @@ export default new Vuex.Store({
       }
     },
     searchItems({ commit }, payload) {
-      payload.length >= 3 ? commit("setFoundItems", payload) : commit("setFoundItems", '')
+      payload.length >= 3
+        ? commit("setFoundItems", payload)
+        : commit("setFoundItems", "");
     },
 
-    pushToCart({commit}, payload){
-      {commit}
-      commit("setCart",payload)
+    pushToCart({ commit }, payload) {
+      {
+        commit;
+      }
+      commit("setCart", payload);
       // console.log(state.carts)
     },
-    removeCart({commit},payload){
-      commit("removeCart",payload)
+    removeCart({ commit }, payload) {
+      commit("removeCart", payload);
     },
-    increaseCount({commit},payload){
-      commit("totalCount",payload)
-    }
-
+    increaseCount({ commit }, payload) {
+      commit("totalCount", payload);
+    },
   },
   mutations: {
-    totalCount(state,payload){
-      const itemIndex = state.items.findIndex(x => x.id == payload.id);
-      if(payload.increase){
-        state.items[itemIndex].total += 1
-      }
-      else{
-        state.items[itemIndex].total -= 1
+    totalCount(state, payload) {
+      const itemIndex = state.items.findIndex((x) => x.id == payload.id);
+      if (payload.increase) state.items[itemIndex].total += 1;
+      else {
+        state.items[itemIndex].total -= 1;
+        if (state.items[itemIndex].total < 0) state.items[itemIndex].total = 0;
       }
       localStorage.setItem("data", JSON.stringify(state.items));
     },
-    removeCart(state,payload){
-      const itemIndex = state.items.findIndex(x => x.id == payload.id);
-      state.items[itemIndex].selected = false
-      state.items[itemIndex].total = 0
+    removeCart(state, payload) {
+      const itemIndex = state.items.findIndex((x) => x.id == payload.id);
+      state.items[itemIndex].selected = false;
+      state.items[itemIndex].total = 0;
       localStorage.setItem("data", JSON.stringify(state.items));
       // state.carts.push(payload)
     },
-    setCart(state,payload){
+    setCart(state, payload) {
       // console.log(state.carts)
-      const itemIndex = state.items.findIndex(x => x.id == payload.id);
-      state.items[itemIndex].selected = true
-      state.items[itemIndex].total +=1
+      const itemIndex = state.items.findIndex((x) => x.id == payload.id);
+      state.items[itemIndex].selected = true;
+      state.items[itemIndex].total += 1;
       localStorage.setItem("data", JSON.stringify(state.items));
       // state.carts.push(payload)
     },
@@ -82,7 +86,12 @@ export default new Vuex.Store({
       const cache = localStorage.getItem("data");
       if (cache) state.originalItems = JSON.parse(cache);
       else {
-        state.originalItems = items.map((v) => ({   ...v,  price: getRandomInt(18000, 100000), selected : false,total:0 }));
+        state.originalItems = items.map((v) => ({
+          ...v,
+          price: getRandomInt(18000, 100000),
+          selected: false,
+          total: 0,
+        }));
         localStorage.setItem("data", JSON.stringify(state.originalItems));
       }
       state.items = state.originalItems;
