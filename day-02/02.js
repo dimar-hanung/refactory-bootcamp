2b814eca91b6c959321b759ef75a4784af322302
dimarhanung <dimarhanung@ecampus.ut.ac.id>
2021-02-11 17:20:33 +0700

    refactor: combineArray

--- day-02/02.js
+++ day-02/02.js
@@ -1,27 +1,26 @@
-const first = ['Behind', 'every', 'great', 'man']
-const second = ['is', 'a', 'woman']
-const third = ['rolling', 'her', 'eyes']
+/**
+ * @description
+ * - 📝 Menggabungkan Array ( Merge )
+ */
 
+const first = ["Behind", "every", "great", "man"];
+const second = ["is", "a", "woman"];
+const third = ["rolling", "her", "eyes"];
 
-
-const output = `${first.join(' ')} ${second.join(' ')} ${third.join(' ')}`;
+/**
+ * Cara Satu, Gabungkan Manual mengguankan template l
+ */
+const output = `${first.join(" ")} ${second.join(" ")} ${third.join(" ")}`;
 console.log(output);
 
+// Cara Dua, Buat Funtion, gunakan forEach
+const combineArray = (...arr) => {
+  let output = [];
+  arr.forEach(arr => (output = [...output, ...arr]));
+  return output.join(" ");
+};
+console.log(combineArray(first, second, third));
 
-// Memberikan function supaya lebih dinamis
-const combine = (...arr) => {
-    let output = ''
-    arr.forEach(arr => output += arr.join(' ') + " ");
-    console.log(output);
-}
-
-combine(first,second,third);
-
-const gabung = (...arr) => {
-    let output = [];
-    console.log(arr)
-    console.log(output.concat(arr).join(" "))
-}
-
-
-gabung(first,second,third)
+// Cara Tiga, Buat Function, gunakan concat untuk menggabungkan array, dan paling efisien
+const combineArray2 = (...arr) => [].concat(...arr).join(" ");
+console.log(combineArray2(first, second, third));
