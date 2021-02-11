d167264155b2e67122868353c4499124b62c7a6a
dimarhanung <dimarhanung@ecampus.ut.ac.id>
2021-02-11 19:00:13 +0700

    fix: lint

--- day-02/04.js
+++ day-02/04.js
@@ -1,15 +1,11 @@
+const leapYear = (year1, year2) => {
+  while (year1 <= year2) {
+    let isLeap = new Date(year1, 1, 29).getDate() === 29;
+    if (isLeap) console.log(`${year1} adalah tahun kabisat`);
+    else console.log(`${year1} adalah bukan tahun kabisat`);
 
-const leapYear = (year1,year2) =>{
-    while(year1 <= year2){
-       let isLeap = new Date(year1, 1, 29).getDate() === 29;
-       if(isLeap == true) console.log(`${year1} adalah tahun kabisat`);
-           else console.log(`${year1} adalah bukan tahun kabisat`)
-       
-       year1++  
+    year1++;
+  }
+};
 
-   }
-
-
-}
-
-leapYear(2001,2020);
+leapYear(2001, 2020);
