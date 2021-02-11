a0c581f6d335a9f3bb046a8ea4364c5ee012cd3a
dimarhanung <dimarhanung@ecampus.ut.ac.id>
2021-02-11 18:59:27 +0700

    refactor: forEach to for of

--- day-02/03.js
+++ day-02/03.js
@@ -1,36 +1,27 @@
-const csv =`NAME, CATEGORY, PRICE
+const csv = `NAME, CATEGORY, PRICE
 Xiaomi Redmi 5A, Smartphone, 1199000
 Macbook Air, Laptop, 13775000
 Samsung Galaxy J7, Smartphone, 3549000
 DELL XPS 13, Laptop, 26799000
 Xiaomi Mi 6, Smartphone, 5399000
-LG V30 Plus, Smartphone, 10499000`
+LG V30 Plus, Smartphone, 10499000`;
 
+const makeObject = data => {
+  let row = data.split("\n");
+  let key = row[0].split(", ");
+  let arrObject = [];
 
+  for (i in row) {
+    let obj = {};
+    let col = row[i].split(", ");
 
-
-const makeObject = (data) => {
-    
-    let row = csv.split("\n");
-    let key = row[0].split(', ');
-    let objOutput = []
-    
-    for(let i = 0;i<row.length;i++){
-        let obj = {}
-        let col = row[i].split(", ")
-
-        for(let j = 0;j < col.length; j++){
-
-            obj[key[j].toLowerCase()] = col[j]
-            
-        }
-
-        objOutput.push(obj)
-        
+    for (j in col) {
+      obj[key[j].toLowerCase()] = col[j];
     }
 
-    console.log(objOutput);
+    arrObject.push(obj);
+  }
+  return arrObject;
+};
 
-}
-
-makeObject(csv);
+console.log(makeObject(csv));
